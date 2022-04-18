d3db73732f018d73d712b122b81335fa30773e72
Mehul Rastogi <67681485+sm0se@users.noreply.github.com>
2022-04-18 20:24:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-
 let pagenr = 1;
-
-
 async function bb(pagenr){
-    const response = await fetch(`https://www.breakingbadapi.com/api/characters/${pagenr}`);
-    const json = await response.json();
+ 
+
+    const data = await fetch('https://www.breakingbadapi.com/api/characters?page=1');
+    const json = await data.json();
     console.log(json);
 
-    const box = document.createElement("div");
-        box.innerHTML=`
-        <img src=${JSON.img}>
-        <h2>${JSON.char_id}</h2>
-        <p>${JSON.name}</p>
-        <p>${JSON.birthday}</p>
-        <p>${JSON.nickname}</p>
 
-        `;
+    const box = document.createElement("div");
+    box.classList.add("box");
+    document.body.appendChild(box);
+    box.appendChild(document.createElement("img")).src = json[pagenr].img;
+
+    const box2 = document.createElement("div");
+    box.classList.add("box2");
+    document.body.appendChild(box2);
+    box.appendChild(document.createElement("h1")).innerHTML = json[pagenr].name;
+    box.appendChild(document.createElement("p")).innerHTML = json[pagenr].status;
+    box.appendChild(document.createElement("p")).innerHTML = json[pagenr].nickname;
+    box.appendChild(document.createElement("p")).innerHTML = json[pagenr].appearance;
+    box.appendChild(document.createElement("p")).innerHTML = json[pagenr].portrayed;
+    box.appendChild(document.createElement("p")).innerHTML = json[pagenr].category;
+    box.appendChild(document.createElement("p")).innerHTML = json[pagenr].better_call_saul_appearance;
+
     document.querySelector(".box").appendChild(box);
+    document.querySelector(".box2").appendChild(box2);
     
 }
 for (i = 1 ; i < 100 ; i++)
